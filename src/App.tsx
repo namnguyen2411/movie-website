fba38905ba3a966f32de51e28a38e3557b9306d7
NPN-PC <phuongnam.nguyen241196@gmail.com>
2025-01-11 20:14:55 +0700

    feat(App): add routing with React Router

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,28 @@
-const App = () => {
-  return <h1 className="text-center text-4xl text-orange-400">App</h1>
+import { BrowserRouter, Route, Routes } from 'react-router'
+// Layout
+import MainLayout from 'src/components/Layout/MainLayout'
+// Pages
+import Home from 'src/pages/Home'
+import MovieDetail from 'src/pages/MovieDetail'
+import MoviesPage from 'src/pages/MoviesPage'
+import NotFound from 'src/pages/NotFound'
+
+export default function App() {
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<MainLayout />}>
+            <Route index element={<Home />} />
+            <Route path="/phim/:id" element={<MovieDetail />} />
+            <Route path="/duyet-tim" element={<MoviesPage />} />
+            <Route path="/phim-bo" element={<MoviesPage />} />
+            <Route path="/phim-le" element={<MoviesPage />} />
+            <Route path="/quoc-gia/:country" element={<MoviesPage />} />
+            <Route path="*" element={<NotFound />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </>
+  )
 }
-export default App
