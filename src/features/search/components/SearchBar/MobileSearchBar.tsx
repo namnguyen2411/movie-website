9c5c9a3631c80e389c86b55a21f6b2f34268fb85
NPN-PC <phuongnam.nguyen241196@gmail.com>
2025-01-21 17:25:39 +0700

    style: upate text color to use foreground variable

--- src/features/search/components/SearchBar/MobileSearchBar.tsx
+++ src/features/search/components/SearchBar/MobileSearchBar.tsx
@@ -9,11 +9,11 @@ const MobileSearchBar = ({ setIsMobileSearchBarOpen }: MobileSearchBarProps) => 
   return (
     <div className="flex h-[80px] flex-1 items-center space-x-4 xl:hidden">
       <div className="relative flex flex-1 items-center rounded-md bg-[#ffffff14]">
-        <Search className="absolute left-3 mr-2 text-white" size={20} />
+        <Search className="absolute left-3 mr-2 text-foreground" size={20} />
         <Input
           type="text"
           placeholder="Nhập tên phim"
-          className="w-full border-transparent py-5 pl-12 pr-4 transition-all duration-300 placeholder:text-white hover:border-white focus:border-white 2xl:w-[380px]"
+          className="w-full border-transparent py-5 pl-12 pr-4 transition-all duration-300 placeholder:text-foreground hover:border-white focus:border-white 2xl:w-[380px]"
         />
       </div>
       <button onClick={() => setIsMobileSearchBarOpen(false)}>
