5db3fbbaf52d3142564869d33c5d8784248f4c39
NPN-PC <phuongnam.nguyen241196@gmail.com>
2025-02-20 12:24:50 +0700

    feat(features/movie/components/TrendingMovies): add grab to slide feature & animation, remove unused elements and tailwind classname

--- src/features/movie/components/TrendingMovies/TrendingMovies.tsx
+++ src/features/movie/components/TrendingMovies/TrendingMovies.tsx
@@ -1,11 +1,15 @@
-import { useEffect, useState } from 'react'
+import { useCallback, useEffect, useState } from 'react'
 import { Link } from 'react-router'
 import { Heart, Info, Play } from 'lucide-react'
 import clsx from 'clsx'
+import useEmblaCarousel from 'embla-carousel-react'
+import { EmblaOptionsType } from 'embla-carousel'
+import Fade from 'embla-carousel-fade'
 import store from 'src/redux/store'
 import { TRENDING_MOVIES } from 'src/constants/trending'
 import { movieApi } from '../../service/movieService'
 import { MovieType } from '../../types/movie.type'
+import { decodeHtml } from 'src/lib/utils'
 
 const fetchmovies = async () => {
   const result = await Promise.all(
@@ -15,10 +19,24 @@ const fetchmovies = async () => {
 }
 
 const TrendingMovies = () => {
+  const OPTIONS: EmblaOptionsType = { loop: true }
+  const [emblaRef, emblaApi] = useEmblaCarousel(OPTIONS, [Fade()])
+
   const [movieList, setMovieList] = useState<MovieType[]>([])
   const [thumbList, setThumbList] = useState<string[]>([])
   const [currentThumbIndex, setCurrentThumbIndex] = useState(0)
   const [windowWidth, setWindowWidth] = useState(window.innerWidth)
+
+  // update index when carousel change
+  const onSelect = useCallback(() => {
+    if (!emblaApi) return
+    setCurrentThumbIndex(emblaApi.selectedScrollSnap())
+  }, [emblaApi])
+
+  useEffect(() => {
+    if (!emblaApi) return
+    emblaApi.on('select', onSelect)
+  }, [emblaApi, onSelect])
 
   useEffect(() => {
     const fetchData = async () => {
@@ -55,105 +73,92 @@ const TrendingMovies = () => {
     <>
       <div
         className={clsx(
-          'inset-0 flex min-h-[300px] flex-col overflow-hidden text-foreground transition-all duration-300',
+          'inset-0 flex min-h-[300px] flex-col overflow-hidden text-foreground',
           'sm:h-[420px]',
           'md:absolute md:h-[700px]',
           'xl:h-[760px]'
         )}
       >
-        {/* Background Image - Only display on 1280px+++ screen */}
-        <div
-          className="absolute inset-0 hidden bg-cover bg-center bg-no-repeat xl:block"
-          style={{
-            backgroundImage: `
-        radial-gradient(rgba(0, 0, 0, 0) 20%, rgba(25, 27, 36, 1) 100%),
-        url(${thumbList[currentThumbIndex]})
-      `
-          }}
-        />
-
         {/* Movie Content */}
         <div
           className={clsx(
-            'flex h-auto min-h-[240px] flex-col bg-cover bg-center bg-no-repeat transition-all duration-300',
+            'image-container relative flex h-auto min-h-[240px] flex-col overflow-hidden',
             'sm:aspect-[16/9] sm:max-h-[320px] sm:gap-2.5',
             'md:max-h-[600px] md:min-h-[450px] md:gap-3.5',
-            'xl:min-h-[660px] xl:!bg-none'
+            'xl:min-h-[660px] xl:!bg-none',
+            'bg-cover bg-center bg-no-repeat'
           )}
           id="movie-content"
-          style={{
-            backgroundImage: `
-      radial-gradient(circle at top, rgba(0, 0, 0, 0) 20%, rgba(25, 27, 36, 1) 100%),
-      url(${thumbList[currentThumbIndex]})
-    `
-          }}
+          ref={emblaRef}
         >
-          {/* Title */}
-          <Link
-            to={`phim/${currentRecommend.slug}`}
-            className={clsx(
-              'mt-auto translate-y-1 bg-gradient-to-b from-main-bg/5 to-main-bg/40 px-mobile pb-2 text-center md:px-medium xl:px-desktop',
-              'sm:w-fit sm:translate-y-0 sm:from-transparent sm:to-transparent sm:pb-0 sm:text-left',
-              'xl:pb-5'
-            )}
-          >
-            <h1 className="text-[22px] font-bold sm:text-[26px] lg:text-3xl 2xl:text-5xl">{currentRecommend.name}</h1>
-          </Link>
-
-          {/* Info */}
-          <div
-            className={clsx(
-              'flex translate-y-1 items-center justify-center space-x-2 bg-gradient-to-b from-main-bg/40 to-main-bg/70 pl-0 text-xs',
-              'sm:translate-y-0 sm:justify-start sm:from-transparent sm:to-transparent sm:px-mobile md:px-medium xl:px-desktop',
-              '2xl:text-sm'
-            )}
-          >
-            <div className="rounded-md border border-yellow px-2 py-1 font-medium text-foreground">
-              <span className="font-normal text-yellow">IMDb</span> 6.3
-            </div>
-            <span className="rounded-md border border-white px-2 py-1">{currentRecommend.year}</span>
-            <span className="rounded-md border border-white px-2 py-1">{currentRecommend.episode_current}</span>
-          </div>
-
-          {/* Category Tags */}
-          <div
-            className={clsx(
-              'hidden translate-y-1 flex-wrap gap-2 bg-gradient-to-b from-transparent to-sub-bg px-mobile text-xs md:px-medium xl:px-desktop',
-              'sm:flex sm:translate-y-0',
-              'md:bg-transparent md:from-transparent md:to-transparent',
-              '2xl:text-sm'
-            )}
-          >
-            {currentRecommend.category.map((c) => (
-              <Link
-                to={`the-loai/${c.slug}`}
-                key={c.slug}
-                className="rounded-md bg-white/20 px-2 py-1 hover:text-yellow"
-              >
-                <span>{c.name}</span>
+          {/* Poster */}
+          <div className="absolute inset-0 flex">
+            {movieList.map((movie) => (
+              <Link to={`phim/${currentRecommend.slug}`} key={movie._id} className="w-full flex-none lg:cursor-grab">
+                <img
+                  src={movie.thumb_url}
+                  alt={movie.name}
+                  className="h-full w-full object-cover transition-all duration-300"
+                />
               </Link>
             ))}
           </div>
-
-          {/* Description */}
+          {/* Movie Info Container */}
           <div
             className={clsx(
-              'z-10 hidden w-[60%] bg-gradient-to-b from-transparent to-sub-bg px-mobile md:px-medium xl:px-desktop',
-              'md:block md:py-2',
-              'xl:w-[50%] xl:!bg-none xl:py-3',
-              '2xl:w-[45%] 2xl:py-4'
+              'z-10 mt-auto cursor-grab space-y-3 bg-gradient-to-b from-main-bg/10 via-main-bg/70 to-main-bg px-mobile md:space-y-4 md:px-medium xl:space-y-5 xl:px-desktop',
+              'lg:from-transparent lg:via-main-bg/70 lg:to-main-bg'
             )}
           >
-            <p className="text-sm md:!line-clamp-3">{currentRecommend.content}</p>
+            {/* Title */}
+            <Link to={`phim/${currentRecommend.slug}`} className="text-center sm:inline-block sm:text-left">
+              <h1
+                key={currentRecommend._id}
+                className="animate-fade-in text-[22px] font-bold hover:text-yellow sm:w-fit sm:text-[26px] lg:text-3xl 2xl:text-4xl"
+              >
+                {currentRecommend.name}
+              </h1>
+            </Link>
+
+            {/* Year & Episode/Rating */}
+            <div
+              className={clsx(
+                'flex select-none items-center justify-center space-x-2 pl-0 text-xs',
+                'sm:justify-start',
+                '2xl:text-sm'
+              )}
+            >
+              <span className="rounded-md border border-white px-2 py-1">{currentRecommend.year}</span>
+              <span className="rounded-md border border-white px-2 py-1">{currentRecommend.episode_current}</span>
+            </div>
+
+            {/* Category Tags */}
+            <div className="hidden flex-wrap gap-2 text-xs sm:flex 2xl:text-sm">
+              {currentRecommend.category.map((c) => (
+                <Link
+                  to={`the-loai/${c.slug}`}
+                  key={c.slug}
+                  className="animate-fade-in select-none rounded-md bg-white/20 px-2 py-1 hover:text-yellow"
+                >
+                  <span>{c.name}</span>
+                </Link>
+              ))}
+            </div>
+
+            {/* Description */}
+            <div className="hidden w-[55%] select-none pb-1 md:block xl:w-[50%] xl:pb-2 2xl:w-[45%] 2xl:pb-4">
+              <p key={currentRecommend._id} className="animate-fade-in text-sm md:!line-clamp-3">
+                {decodeHtml(currentRecommend.content)}
+              </p>
+            </div>
           </div>
         </div>
 
         {/* Actions & Carousel */}
         <div
           className={clsx(
-            'z-10 !ml-0 flex h-[60px] w-full translate-y-1 items-center justify-between bg-gradient-to-b from-main-bg/70 to-main-bg px-mobile md:px-medium xl:px-desktop',
-            'sm:h-[100px] sm:translate-y-0 sm:bg-sub-bg sm:from-transparent sm:to-transparent',
-            'xl:bg-transparent'
+            'z-10 !ml-0 flex h-[60px] w-full items-center justify-between px-mobile md:px-medium xl:px-desktop',
+            'sm:h-[100px] sm:from-main-bg sm:via-sub-bg/50 sm:to-sub-bg'
           )}
         >
           {/* Actions */}
@@ -193,9 +198,9 @@ const TrendingMovies = () => {
             {thumbList.map((thumb, index) => (
               <button
                 key={index}
-                onClick={() => setCurrentThumbIndex(index)}
+                onClick={() => emblaApi?.scrollTo(index)}
                 className={clsx(
-                  'h-[26px] w-[40px] overflow-hidden rounded-md border-2 hover:border-white',
+                  'h-[26px] w-[40px] overflow-hidden rounded-md border-2 transition-all duration-300 hover:border-white',
                   'sm:h-[30px] sm:w-[45px]',
                   'lg:h-[40px] lg:w-[60px]',
                   '2xl:h-[40px] 2xl:w-[80px]',
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,19 @@ import { twMerge } from 'tailwind-merge'
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export function convertMinutesToHours(input: string): string {
+  const minutes = parseInt(input.replace(' phút', ''), 10)
+
+  const hours = Math.floor(minutes / 60)
+  const remainingMinutes = minutes % 60
+
+  const formattedMinutes = remainingMinutes < 10 ? `0${remainingMinutes}` : remainingMinutes
+
+  return `${hours}h ${formattedMinutes}m`
+}
+
+export const decodeHtml = (input: string): string => {
+  const doc = new DOMParser().parseFromString(input, 'text/html')
+  return doc.documentElement.textContent ?? ''
+}
